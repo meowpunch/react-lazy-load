faab061efbe1e03436666bd9dad47cc3c7f5ad27
meowpunch <jinhoonbae@gmail.com>
2021-12-19 09:29:40 +0100

    Grid

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 
 Loading all the images in one page at once worse makes user experience worse. While scrolling, render only images in the intersection with [Viewport](https://developer.mozilla.org/en-US/docs/Glossary/Viewport)
 
+- React & Typescript
 - [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
-
 
 ## Requirements
 - spread out images within Grid.
 - apply lazy loading to images.
 
+
+
+
+## Get Started
+
+- Node v17.2.0
+- npm v8.1.4
+ 
+```shell
+# install all the dependencies
+npm install
+
+# run app
+npm start
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Images from './constant/Images';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Image = (url: string) => {
+    return (
+        <img src={url} alt="dummy"/>
+    )
+}
+
+
+/*
+    - chunk by col
+    - spread them in Grid
+ */
+interface GridProps {
+    arr: JSX.Element[]
+    c: number
+}
+
+const chunk = <T, >(arr: T[], size: number) =>
+    [...Array(Math.ceil(arr.length / size))].map((_, i) => arr.slice(i * size, i * size + size));
+
+const Grid = (props: GridProps) => {
+    const {arr, c} = props;
+
+    const chunks: JSX.Element[][] = chunk(arr, c);
+
+    return (
+        <div>
+            {chunks.map(c => <div>{c}</div>)}
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="App">
+            <Grid
+                arr={Images.buildImages(30).map(url => {
+                    console.log(url)
+                    return Image(url)
+                })}
+                c={3}
+            />
+        </div>
+    );
 }
 
 export default App;
